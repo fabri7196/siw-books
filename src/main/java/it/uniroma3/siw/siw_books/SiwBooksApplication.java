c2a0070225c23133cb9ee23efbf0f412be3abfcf
fabri7196 <fabry_c96@hotmail.it>
2025-05-31 18:27:34 +0200

    first commit

---
+++ src/main/java/it/uniroma3/siw/siw_books/SiwBooksApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siw_books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/siw_books/SiwBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siw_books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
