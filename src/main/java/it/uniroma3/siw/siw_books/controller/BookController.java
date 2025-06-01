00f3f7acb4eadacd6ee0c400b4be0c87ac2fbab7
fabri7196 <fabry_c96@hotmail.it>
2025-06-01 13:48:16 +0200

    Modificato BookController.java e creata vista base per Homepage.

--- src/main/java/it/uniroma3/siw/siw_books/controller/BookController.java
+++ src/main/java/it/uniroma3/siw/siw_books/controller/BookController.java
@@ -4,10 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import it.uniroma3.siw.siw_books.service.BookService;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BookController {
 
     @Autowired BookService bookService;
+    
+    @GetMapping("/")
+    public String getHome() {
+        return "index.html";
+    }
     
 }
